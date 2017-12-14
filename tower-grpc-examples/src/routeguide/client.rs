6f76199d077779f86419e130ee05db3089231556
Carl Lerche <me@carllerche.com>
2017-12-14 15:25:40 -0600

    Start working on routeguide client

--- tower-grpc-examples/src/routeguide/client.rs
+++ tower-grpc-examples/src/routeguide/client.rs
@@ -4,7 +4,7 @@
 extern crate bytes;
 extern crate env_logger;
 extern crate futures;
-#[macro_use]
+extern crate http;
 extern crate log;
 extern crate prost;
 #[macro_use]
@@ -19,11 +19,47 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use futures::Future;
+use tokio_core::reactor::Core;
+use tokio_core::net::TcpStream;
+use tower_grpc::Request;
+use tower_h2::client::Connection;
+
+use routeguide::Point;
+
 mod data;
 pub mod routeguide {
     include!(concat!(env!("OUT_DIR"), "/routeguide.rs"));
 }
 
 pub fn main() {
-    unimplemented!();
+    let _ = ::env_logger::init();
+
+    let mut core = Core::new().unwrap();
+    let reactor = core.handle();
+
+    let addr = "127.0.0.1:10000".parse().unwrap();
+    let uri: http::Uri = format!("http://localhost:10000").parse().unwrap();
+
+    let mut client = core.run({
+        TcpStream::connect(&addr, &reactor)
+            .and_then(move |socket| {
+                // Bind the HTTP/2.0 connection
+                Connection::handshake(socket, reactor)
+                    .map_err(|_| panic!("failed HTTP/2.0 handshake"))
+            })
+            .and_then(move |conn| {
+                use routeguide::client::RouteGuide;
+                Ok(RouteGuide::new(conn, uri))
+            })
+    }).unwrap();
+
+    let response = core.run({
+        client.get_feature(Request::new(Point {
+            latitude: 409146138,
+            longitude: -746188906,
+        }))
+    }).unwrap();
+
+    println!("GetFeature Response = {:?}", response);
 }
