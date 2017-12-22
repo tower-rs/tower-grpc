ddc482a243b50223239c6d6371982317da1f07de
Eliza Weisman <eliza@buoyant.io>
2017-12-22 11:00:12 -0800

    prettyprint protoc errors in examples

--- tower-grpc-examples/build.rs
+++ tower-grpc-examples/build.rs
@@ -6,19 +6,19 @@ fn main() {
         .enable_server(true)
         .enable_client(true)
         .build(&["proto/helloworld/helloworld.proto"], &["proto/helloworld"])
-        .unwrap();
+        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 
     // Build routeguide
     tower_grpc_build::Config::new()
         .enable_server(true)
         .enable_client(true)
         .build(&["proto/routeguide/route_guide.proto"], &["proto/routeguide"])
-        .unwrap();
+        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 
     // Build grpc-interop
     tower_grpc_build::Config::new()
         .enable_server(true)
         .enable_client(true)
         .build(&["proto/grpc/testing/test.proto"], &["proto/grpc/testing"])
-        .unwrap();
+        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 }
