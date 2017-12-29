7505f8628898fc5ebc5fda1e8b667ad2bfbc16a5
Eliza Weisman <eliza@buoyant.io>
2017-12-29 14:02:05 -0800

    framework for making test assertions, nicer output

--- tower-grpc-examples/src/interop/client.rs
+++ tower-grpc-examples/src/interop/client.rs
@@ -1,3 +1,5 @@
+
+extern crate console;
 #[macro_use]
 extern crate clap;
 extern crate env_logger;
@@ -13,11 +15,14 @@ extern crate tower;
 extern crate tower_h2;
 extern crate tower_grpc;
 
+use std::error::Error;
+use std::fmt;
 use std::net::{IpAddr, SocketAddr};
-use futures::Future;
+
+use futures::{future, Future};
 use tokio_core::reactor::Core;
 use tokio_core::net::TcpStream;
-use tower_grpc::Request;
+use tower_grpc::{Request, Response};
 use tower_h2::client::Connection;
 
 mod pb {
@@ -58,6 +63,93 @@ arg_enum!{
         concurrent_large_unary
     }
 }
+
+enum TestAssertion {
+    Passed { description: &'static str },
+    Failed { description: &'static str, 
+             expression: &'static str, 
+             why: Option<String> },
+    Errored { description: &'static str, error: Box<Error> }
+}
+
+impl fmt::Display for TestAssertion {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use console::{Emoji, style};
+        match *self {
+            TestAssertion::Passed { ref description } =>
+                write!(f, "{check} {desc}", 
+                    check = style(Emoji("✔", "+")).green(),
+                    desc = style(description).green(),
+                ),
+            TestAssertion::Failed { 
+                ref description,
+                ref expression,
+                why: Some(ref why),
+            } =>
+                write!(f, "{check} {desc}\n  in `{exp}`: {why}",
+                    check = style(Emoji("✖", "x")).red(),
+                    desc = style(description).red(),
+                    exp = expression,
+                    why = why,
+                ),
+            TestAssertion::Failed { 
+                ref description,
+                ref expression,
+                why: None,
+            } =>
+                write!(f, "{check} {desc}\n  in `{exp}`",
+                    check = style(Emoji("✖", "x")).red(),
+                    desc = style(description).red(),
+                    exp = expression,
+                ),
+            _ => unimplemented!()
+        }
+        
+    }
+}
+
+// impl TestAssertion {
+//     fn from_err<E: Error>(e: E) -> Self {
+//         TestAssertion::Errored
+//     }
+// }
+
+macro_rules! test_assert {
+    ($description:expr, $assertion:expr) => {
+        if $assertion {
+            TestAssertion::Passed { description: $description }
+        } else {
+            TestAssertion::Failed { 
+                description: $description,
+                expression: stringify!($assertion),
+                why: None
+            }
+        }
+    };
+    ($description:expr, $assertion:expr, $why:expr) => {
+        if $assertion {
+            TestAssertion::Passed { description: $description }
+        } else {
+            TestAssertion::Failed { 
+                description: $description,
+                expression: stringify!($assertion),
+                why: Some($why)
+            }
+        }
+    }; 
+}
+
+
+// impl Testcase {
+//     fn future<S>(self, client: &mut pb::client::TestService<S>) -> TestFuture
+//     where 
+//         S: tower_h2::HttpService,
+//         S::Future: Future,
+//     {
+
+//     }
+        
+// }
 
 struct ServerInfo {
     addr: SocketAddr,
@@ -141,108 +233,174 @@ fn main() {
     let mut core = Core::new().expect("could not create reactor core!");
     let reactor = core.handle();
 
-    match test_case {
+    let mut client = core.run(
+            TcpStream::connect(&addr, &reactor)
+                .and_then(move |socket| {
+                    // Bind the HTTP/2.0 connection
+                    Connection::handshake(socket, reactor)
+                        .map_err(|_| panic!("failed HTTP/2.0 handshake"))
+                })
+                .and_then(move |conn| {
+                    use pb::client::TestService;
+                    let client = TestService::new(conn, uri)
+                        .expect("TestService::new");
+                    Ok(client)
+                })
+        ).expect("client");
+    
+    let test = match test_case {
         Testcase::empty_unary => {
-            let test = 
-                TcpStream::connect(&addr, &reactor)
-                    .and_then(move |socket| {
-                        // Bind the HTTP/2.0 connection
-                        Connection::handshake(socket, reactor)
-                            .map_err(|_| panic!("failed HTTP/2.0 handshake"))
-                    })
-                    .and_then(move |conn| {
-                        use pb::client::TestService;
-                        let client = TestService::new(conn, uri)
-                            .expect("TestService::new");
-                        Ok(client)
-                    })
-                    .and_then(|mut client| {
-                        use pb::Empty;
-
-                        client.empty_call(Request::new(Empty {}))
-                            .map_err(|e| panic!("gRPC request failed; err={:?}", e))
-                    })
-                    .and_then(|response| {
-                        println!("RESPONSE = {:?}", response);
-                        Ok(())
-                    })
-                    .map_err(|e| {
-                        println!("ERR = {:?}", e);
-                    });
-            core.run(test).expect("run test");
+            use pb::Empty;
+            core.run(client.empty_call(Request::new(Empty {}))
+                .then(|result| {
+                    let mut assertions = vec![
+                        test_assert!(
+                            "call must be successful",
+                            result.is_ok(),
+                            format!("result={:?}", result)
+                        )
+                    ];
+                    if let Ok(body) = result.map(|r| r.into_inner()) {
+                        assertions.push(test_assert!(
+                            "body must not be null",
+                            body == Empty{},
+                            format!("body={:?}", body)
+                        ))
+                    }
+                    future::ok::<Vec<TestAssertion>, Box<Error>>(assertions)
+                }))
         },
-        // cacheable_unary => {
-        //     let test = 
-        //         TcpStream::connect(&addr, &reactor)
-        //             .and_then(move |socket| {
-        //                 // Bind the HTTP/2.0 connection
-        //                 Connection::handshake(socket, reactor)
-        //                     .map_err(|_| panic!("failed HTTP/2.0 handshake"))
-        //             })
-        //             .and_then(move |conn| {
-        //                 use testing::client::TestService;
-        //                 let client = TestService::new(conn, uri)
-        //                     .expect("TestService::new");
-        //                 Ok(client)
-        //             })
-        //             .and_then(|mut client| {
-        //                 use testing::SimpleRequest;
-
-        //                 client.cacheable_unary(Request::new(SimpleRequest {
-                            
-        //                 }))
-        //                     .map_err(|e| panic!("gRPC request failed; err={:?}", e))
-        //             })
-        //             .and_then(|response| {
-        //                 println!("RESPONSE = {:?}", response);
-        //                 Ok(())
-        //             })
-        //             .map_err(|e| {
-        //                 println!("ERR = {:?}", e);
-        //             });
-        //     core.run(test).expect("run test");
-
-        // },
         Testcase::large_unary => {
-            let test = 
-                TcpStream::connect(&addr, &reactor)
-                    .and_then(move |socket| {
-                        // Bind the HTTP/2.0 connection
-                        Connection::handshake(socket, reactor)
-                            .map_err(|_| panic!("failed HTTP/2.0 handshake"))
-                    })
-                    .and_then(move |conn| {
-                        use pb::client::TestService;
-                        let client = TestService::new(conn, uri)
-                            .expect("TestService::new");
-                        Ok(client)
-                    })
-                    .and_then(|mut client| {
-                        use pb::SimpleRequest;
-                        let payload = util::client_payload(
-                            pb::PayloadType::Compressable,
-                            LARGE_REQ_SIZE,
-                        );
-                        let req = SimpleRequest {
-                            response_type: pb::PayloadType::Compressable as i32,
-                            response_size: LARGE_RSP_SIZE,
-                            payload: Some(payload),
-                            ..Default::default()
-                        };
-                        client.unary_call(Request::new(req))
-                            .map_err(|e| panic!("gRPC request failed; err={:?}", e))
-                    })
-                    .and_then(|response| {
-                        println!("RESPONSE = {:?}", response);
-                        Ok(())
-                    })
-                    .map_err(|e| {
-                        println!("ERR = {:?}", e);
-                    });
-            core.run(test).expect("run test");
+            use pb::SimpleRequest;
+            use std::mem;
+            let payload = util::client_payload(
+                pb::PayloadType::Compressable,
+                LARGE_REQ_SIZE,
+            );
+            let req = SimpleRequest {
+                response_type: pb::PayloadType::Compressable as i32,
+                response_size: LARGE_RSP_SIZE,
+                payload: Some(payload),
+                ..Default::default()
+            };
+            core.run(client.unary_call(Request::new(req))
+                .then(|result| {
+                   let mut assertions = vec![
+                        test_assert!(
+                            "call must be successful",
+                            result.is_ok(),
+                            format!("result={:?}", result)
+                        )
+                   ];
+                    if let Ok(body) = result.map(|r| r.into_inner()) {
+                        assertions.push(test_assert!(
+                        "body must be 314159 bytes",
+                        mem::size_of_val(&body) == LARGE_RSP_SIZE as usize,
+                        format!("mem::size_of_val(&body)={:?}", 
+                            mem::size_of_val(&body))
+                        ));
+                    }
+                    future::ok::<Vec<TestAssertion>, Box<Error>>(assertions)
+                }))
         },
-        t => unimplemented!("test case {:?} is not yet implemented.", t),
+        Testcase::cacheable_unary => {
+            let payload = pb::Payload {
+                type_: pb::PayloadType::Compressable as i32,
+                body: format!("{:?}", std::time::Instant::now()).into_bytes(),
+            };
+            // let req = SimpleRequest {
+            //     response_type: pb
+            // };
+            unimplemented!()
+        },
+        _ => unimplemented!()
     };
+    
+    let test_results = test.expect("error running test!");
+
+    println!("{:?}:", test_case);
+    for result in test_results {
+        println!("  {}", result);
+    }
+    
+
+    // match test_case {
+    //     Testcase::empty_unary => {
+    //         let test = 
+    //         core.run(test).expect("run test");
+    //     },
+    //     // cacheable_unary => {
+    //     //     let test = 
+    //     //         TcpStream::connect(&addr, &reactor)
+    //     //             .and_then(move |socket| {
+    //     //                 // Bind the HTTP/2.0 connection
+    //     //                 Connection::handshake(socket, reactor)
+    //     //                     .map_err(|_| panic!("failed HTTP/2.0 handshake"))
+    //     //             })
+    //     //             .and_then(move |conn| {
+    //     //                 use testing::client::TestService;
+    //     //                 let client = TestService::new(conn, uri)
+    //     //                     .expect("TestService::new");
+    //     //                 Ok(client)
+    //     //             })
+    //     //             .and_then(|mut client| {
+    //     //                 use testing::SimpleRequest;
+
+    //     //                 client.cacheable_unary(Request::new(SimpleRequest {
+                            
+    //     //                 }))
+    //     //                     .map_err(|e| panic!("gRPC request failed; err={:?}", e))
+    //     //             })
+    //     //             .and_then(|response| {
+    //     //                 println!("RESPONSE = {:?}", response);
+    //     //                 Ok(())
+    //     //             })
+    //     //             .map_err(|e| {
+    //     //                 println!("ERR = {:?}", e);
+    //     //             });
+    //     //     core.run(test).expect("run test");
+
+    //     // },
+    //     Testcase::large_unary => {
+    //         let test = 
+    //             TcpStream::connect(&addr, &reactor)
+    //                 .and_then(move |socket| {
+    //                     // Bind the HTTP/2.0 connection
+    //                     Connection::handshake(socket, reactor)
+    //                         .map_err(|_| panic!("failed HTTP/2.0 handshake"))
+    //                 })
+    //                 .and_then(move |conn| {
+    //                     use pb::client::TestService;
+    //                     let client = TestService::new(conn, uri)
+    //                         .expect("TestService::new");
+    //                     Ok(client)
+    //                 })
+    //                 .and_then(|mut client| {
+    //                     use pb::SimpleRequest;
+    //                     let payload = util::client_payload(
+    //                         pb::PayloadType::Compressable,
+    //                         LARGE_REQ_SIZE,
+    //                     );
+    //                     let req = SimpleRequest {
+    //                         response_type: pb::PayloadType::Compressable as i32,
+    //                         response_size: LARGE_RSP_SIZE,
+    //                         payload: Some(payload),
+    //                         ..Default::default()
+    //                     };
+    //                     client.unary_call(Request::new(req))
+    //                         .map_err(|e| panic!("gRPC request failed; err={:?}", e))
+    //                 })
+    //                 .and_then(|response| {
+    //                     println!("RESPONSE = {:?}", response);
+    //                     Ok(())
+    //                 })
+    //                 .map_err(|e| {
+    //                     println!("ERR = {:?}", e);
+    //                 });
+    //         core.run(test).expect("run test");
+    //     },
+    //     t => unimplemented!("test case {:?} is not yet implemented.", t),
+    // };
 
 
 }
