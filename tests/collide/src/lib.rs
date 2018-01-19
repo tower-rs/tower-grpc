a96f4270503a2604e1c41dbd1333319c82913593
Carl Lerche <me@carllerche.com>
2018-01-19 14:32:43 -0800

    Fix more complicated namespace codegen (#38)

--- tests/collide/src/lib.rs
+++ tests/collide/src/lib.rs
@@ -6,10 +6,18 @@ extern crate tower_grpc;
 
 pub mod common {
     include!(concat!(env!("OUT_DIR"), "/common.rs"));
+
+    pub mod nested {
+        include!(concat!(env!("OUT_DIR"), "/common.nested.rs"));
+    }
 }
 
 pub mod hello {
     include!(concat!(env!("OUT_DIR"), "/hello.rs"));
+
+    pub mod nested {
+        include!(concat!(env!("OUT_DIR"), "/hello.nested.rs"));
+    }
 }
 
 #[cfg(test)]
