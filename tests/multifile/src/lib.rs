c579f08e81dc430e7af082db2ce7807c9c8b56f1
Carl Lerche <me@carllerche.com>
2018-01-22 10:17:44 -0800

    Fix client server streaming codgen
    The existing codegen included an unused generic, which prevents calling
    the actual function given that the generic can not be inferred.

--- tests/multifile/src/lib.rs
+++ tests/multifile/src/lib.rs
@@ -2,6 +2,7 @@ extern crate bytes;
 extern crate prost;
 #[macro_use]
 extern crate prost_derive;
+extern crate tower_h2;
 extern crate tower_grpc;
 
 pub mod hello {
@@ -20,5 +21,25 @@ mod tests {
     fn types_are_present() {
         mem::size_of::<::hello::HelloRequest>();
         mem::size_of::<::world::WorldRequest>();
+    }
+
+    #[test]
+    fn can_call() {
+        use ::hello::{HelloRequest};
+        use ::hello::client::Hello;
+        use ::tower_h2::BoxBody;
+        use ::tower_grpc::codegen::client::*;
+
+        #[allow(dead_code)]
+        fn zomg<T>(client: &mut Hello<T>)
+        where T: tower_h2::HttpService<RequestBody = BoxBody>,
+        {
+            let request = HelloRequest {
+                name: "hello".to_string(),
+            };
+
+            let _ = client.say_hello(grpc::Request::new(request.clone()));
+            let _ = client.say_hello2(grpc::Request::new(request.clone()));
+        }
     }
 }
