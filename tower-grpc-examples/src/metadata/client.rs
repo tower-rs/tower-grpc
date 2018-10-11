34b2e8ff067b7cb58ac45d1efc9e5001bb872448
Franck Royer <royer.franck@gmail.com>
2018-10-12 05:58:45 +1000

    Adding metadata usage example (#80)
    In relation to #79. While it may be straightforward to some that gRPC metadata can directly be set as custom headers in the HTTP2 layer, it was not to me.
    
    I think that adding it in the example may be useful to some until the `metadata` check box is ticked.

---
+++ tower-grpc-examples/src/metadata/client.rs
@@ -0,0 +1,70 @@
+extern crate bytes;
+extern crate env_logger;
+extern crate http;
+extern crate futures;
+extern crate log;
+extern crate prost;
+#[macro_use]
+extern crate prost_derive;
+extern crate tokio_core;
+extern crate tower_h2;
+extern crate tower_http;
+extern crate tower_grpc;
+
+use futures::Future;
+use tokio_core::reactor::Core;
+use tokio_core::net::TcpStream;
+use tower_grpc::Request;
+use tower_h2::client::Connection;
+
+pub mod metadata {
+    include!(concat!(env!("OUT_DIR"), "/metadata.rs"));
+}
+
+pub fn main() {
+    let _ = ::env_logger::init();
+
+    let mut core = Core::new().unwrap();
+    let reactor = core.handle();
+
+    let addr = "[::1]:50051".parse().unwrap();
+    let uri: http::Uri = format!("http://localhost:50051").parse().unwrap();
+
+    let say_hello = TcpStream::connect(&addr, &reactor)
+        .and_then(move |socket| {
+            // Bind the HTTP/2.0 connection
+            Connection::handshake(socket, reactor)
+                .map_err(|_| panic!("failed HTTP/2.0 handshake"))
+        })
+        .map(move |conn| {
+            use metadata::client::Doorman;
+            use tower_http::add_origin;
+
+            let conn = add_origin::Builder::new()
+                .uri(uri)
+                .build(conn)
+                .unwrap();
+
+            Doorman::new(conn)
+        })
+        .and_then(|mut client| {
+            use metadata::EnterRequest;
+
+            let mut request = Request::new(EnterRequest {
+                message: "Hello! Can I come in?".to_string(),
+            });
+
+            request.headers_mut().insert("metadata", "Here is a cookie".parse().unwrap());
+
+            client.ask_to_enter(request).map_err(|e| panic!("gRPC request failed; err={:?}", e))
+        })
+        .and_then(|response| {
+            println!("RESPONSE = {:?}", response);
+            Ok(())
+        })
+        .map_err(|e| {
+            println!("ERR = {:?}", e);
+        });
+
+    core.run(say_hello).unwrap();
+}
