e9c4c8266a5389496f3331702a46b897ba5a84ea
Sean McArthur <sean@seanmonstar.com>
2018-11-26 11:39:45 -0800

    update examples from tokio-core to tokio

--- tower-grpc-examples/src/routeguide/client.rs
+++ tower-grpc-examples/src/routeguide/client.rs
@@ -9,10 +9,12 @@ extern crate log;
 extern crate prost;
 #[macro_use]
 extern crate prost_derive;
-extern crate tokio_core;
+extern crate tokio;
+extern crate tokio_connect;
 extern crate tower_h2;
 extern crate tower_http;
 extern crate tower_grpc;
+extern crate tower_service;
 
 extern crate serde;
 extern crate serde_json;
@@ -20,10 +22,11 @@ extern crate serde_json;
 extern crate serde_derive;
 
 use futures::Future;
-use tokio_core::reactor::Core;
-use tokio_core::net::TcpStream;
+use tokio::executor::DefaultExecutor;
+use tokio::net::tcp::{ConnectFuture, TcpStream};
 use tower_grpc::Request;
-use tower_h2::client::Connection;
+use tower_h2::client;
+use tower_service::MakeService;
 
 use routeguide::Point;
 
@@ -35,38 +38,51 @@ pub mod routeguide {
 pub fn main() {
     let _ = ::env_logger::init();
 
-    let mut core = Core::new().unwrap();
-    let reactor = core.handle();
 
-    let addr = "127.0.0.1:10000".parse().unwrap();
     let uri: http::Uri = format!("http://localhost:10000").parse().unwrap();
 
-    let mut client = core.run({
-        TcpStream::connect(&addr, &reactor)
-            .and_then(move |socket| {
-                // Bind the HTTP/2.0 connection
-                Connection::handshake(socket, reactor)
-                    .map_err(|_| panic!("failed HTTP/2.0 handshake"))
-            })
-            .map(move |conn| {
-                use routeguide::client::RouteGuide;
-                use tower_http::add_origin;
+    let h2_settings = Default::default();
+    let mut make_client = client::Connect::new(Dst, h2_settings, DefaultExecutor::current());
 
-                let conn = add_origin::Builder::new()
-                    .uri(uri)
-                    .build(conn)
-                    .unwrap();
+    let rg = make_client.make_service(())
+        .map(move |conn| {
+            use routeguide::client::RouteGuide;
+            use tower_http::add_origin;
 
-                RouteGuide::new(conn)
-            })
-    }).unwrap();
+            let conn = add_origin::Builder::new()
+                .uri(uri)
+                .build(conn)
+                .unwrap();
 
-    let response = core.run({
-        client.get_feature(Request::new(Point {
-            latitude: 409146138,
-            longitude: -746188906,
-        }))
-    }).unwrap();
+            RouteGuide::new(conn)
+        })
+        .and_then(|mut client| {
+            client
+                .get_feature(Request::new(Point {
+                    latitude: 409146138,
+                    longitude: -746188906,
+                }))
+                .map_err(|e| panic!("gRPC request failed; err={:?}", e))
+        })
+        .map(|response| {
+            println!("RESPONSE = {:?}", response);
+        })
+        .map_err(|e| {
+            println!("ERR = {:?}", e);
+        });
 
-    println!("GetFeature Response = {:?}", response);
+    tokio::run(rg);
 }
+
+struct Dst;
+
+impl tokio_connect::Connect for Dst {
+    type Connected = TcpStream;
+    type Error = ::std::io::Error;
+    type Future = ConnectFuture;
+
+    fn connect(&self) -> Self::Future {
+        TcpStream::connect(&([127, 0, 0, 1], 10000).into())
+    }
+}
+
