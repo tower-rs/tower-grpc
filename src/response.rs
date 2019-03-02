1a737abf0a1d000d99d19358c1140a6601508389
Sean McArthur <sean@seanmonstar.com>
2019-03-01 21:55:05 -0800

    Add metadata to Response (#121)

--- src/response.rs
+++ src/response.rs
@@ -1,53 +1,72 @@
 use http;
+use metadata::MetadataMap;
 
+/// A gRPC response and metadata from an RPC call.
 #[derive(Debug)]
 pub struct Response<T> {
-    http: http::Response<T>,
+    metadata: MetadataMap,
+    message: T,
 }
 
 impl<T> Response<T> {
+    /// Create a new gRPC response.
     pub fn new(message: T) -> Self {
-        let mut res = http::Response::new(message);
-        *res.version_mut() = http::Version::HTTP_2;
-
         Response {
-            http: res,
+            metadata: MetadataMap::new(),
+            message,
         }
     }
 
     /// Get a reference to the message
     pub fn get_ref(&self) -> &T {
-        self.http.body()
+        &self.message
     }
 
     /// Get a mutable reference to the message
     pub fn get_mut(&mut self) -> &mut T {
-        self.http.body_mut()
+        &mut self.message
+    }
+
+    /// Get a reference to the custom response metadata.
+    pub fn metadata(&self) -> &MetadataMap {
+        &self.metadata
+    }
+
+    /// Get a mutable reference to the response metadata.
+    pub fn metadata_mut(&mut self) -> &mut MetadataMap {
+        &mut self.metadata
     }
 
     /// Consumes `self`, returning the message
     pub fn into_inner(self) -> T {
-        let (_, body) = self.http.into_parts();
-        body
+        self.message
     }
 
     pub(crate) fn from_http(res: http::Response<T>) -> Self {
+        let (head, message) = res.into_parts();
         Response {
-            http: res,
+            metadata: MetadataMap::from_headers(head.headers),
+            message,
         }
     }
 
     pub fn into_http(self) -> http::Response<T> {
-        self.http
+        let mut res = http::Response::new(self.message);
+
+        *res.version_mut() = http::Version::HTTP_2;
+        *res.headers_mut() = self.metadata.into_headers();
+
+        res
     }
 
     pub fn map<F, U>(self, f: F) -> Response<U>
     where F: FnOnce(T) -> U,
     {
-        let (head, body) = self.http.into_parts();
-        let body = f(body);
-        let http = http::Response::from_parts(head, body);
-        Response::from_http(http)
+        let message = f(self.message);
+        Response {
+            metadata: self.metadata,
+            message,
+        }
     }
 
     // pub fn metadata()
